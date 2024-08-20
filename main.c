c5990036e35f5f343f8ba241f8e919a83b9f8371
orientiered <da.morgachev@mail.ru>
2024-08-20 15:59:47 +0300

    enum for exit codes, some fixes

--- main.c
+++ main.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #define EPSILON 1e-10 //constant for comparing floats
 #define EXIT_BAD_INPUT 1 //exit when input can't be parsed correctly
 
 void printKvadr(double a, double b, double c); //output equation in nice format
+
 double fixMinusZero(double num); //fix -0
 
+enum solutionCode { // exit codes in solution struct
+    ZERO_ROOTS,
+    ONE_ROOT,
+    TWO_ROOTS,
+    INF_ROOTS,
+    BLANK_ROOT = -1
+};
+
 typedef struct solution{
-    int code = 0; //exit code, =number of found roots
-    double x1 = 0;
-    double x2 = 0;
+    enum solutionCode code; //exit code, =number of found roots
+    double x1;
+    double x2;
 } solution_t;
+
+
+
+
+const solution_t BLANK_SOLUTION = {BLANK_ROOT, NAN, NAN};
+
 
 solution_t solve(double a, double b, double c); //solves equation, prints some comments and returns struct with answers
 
 
 int main(int argc, char *argv[]) {
+    printf("# Quadratic equation solver\n# orientiered 2024\n");
     if (argc != 4)
         printf("Enter coefficients of equation ax^2 + bx + c = 0\n");
+
     double a, b, c;
     a = b = c = 0;
+
     if (argc == 4) { //getting input from cmd args
         a = atof(*++argv);
         b = atof(*++argv);
@@ -33,9 +52,27 @@ int main(int argc, char *argv[]) {
 
     printKvadr(a, b, c);
     solution_t result = solve(a, b, c);
-    if (result.code == 1) printf("x = %lg\n", fixMinusZero(result.x1));
-    else if (result.code == 2) printf("x1 = %lg\nx2 = %lg\n", fixMinusZero(result.x1), fixMinusZero(result.x2));
 
+    switch(result.code) {
+        case BLANK_ROOT:
+            printf("Something went wrong\n");
+            break;
+        case ZERO_ROOTS:
+            break;
+        case ONE_ROOT:
+            printf("x = %lg\n", result.x1);
+            break;
+        case TWO_ROOTS:
+            printf("x1 = %lg\nx2 = %lg\n", result.x1, result.x2);
+            break;
+        case INF_ROOTS:
+            printf("x is any number");
+            break;
+        default:
+            printf("That's really bad :(\n");
+            break;
+
+    }
     return 0;
 }
 
@@ -57,41 +94,52 @@ void printKvadr(double a, double b, double c) {
 
     if (!(printedBefore && fabs(c) < EPSILON)) { //x + 0 <=> x
         if (printedBefore) printf((c < 0) ? "- %g " : "+ %g ", fabs(c));
-        else printf("%g", c);
+        else printf("%g ", c);
     }
     printf("= 0\n");
 
 }
 
 solution_t solve(double a, double b, double c) {
-    solution_t result;
+
+    assert(std::isfinite(a)); //checking input for NaNs
+    assert(std::isfinite(b));
+    assert(std::isfinite(c));
+
+    solution_t result = BLANK_SOLUTION;//={};
 
     if (fabs(a) < EPSILON) { //checking for zeros in coefficients; we divide only by a, so this check is essential
-        //printf("Linear equation detected\n");
         //a = 0
         if (fabs(b) < EPSILON) {
             //b = 0
-            if (fabs(c) < EPSILON) // 0 = 0
-                printf("x is any number\n");
-            else
+            if (fabs(c) < EPSILON) { // 0 = 0
+                result.code = INF_ROOTS;
+            } else {
                 printf("Equation can't be solved\n");
+                result.code = ZERO_ROOTS;
+            }
         } else {
-            result.code = 1;
+            result.code = ONE_ROOT;
             result.x1 = -c/b;
         }
     } else {
         double D = b*b - 4*a*c; //calculating discriminant
         if (fabs(D) < EPSILON) {            //D = 0
-            result.code = 1;
+            result.code = ONE_ROOT;
             result.x1 = -b / (2*a);
-        } else if (D < 0) {                 //D < 0
+        } else if (D < 0) {
+            result.code = ZERO_ROOTS;              //D < 0
             printf("Equation can't be solved in R: D = %g < 0\n", D);
         } else {                            //D > 0
-            result.code = 2;
-            result.x1 = (-b + sqrt(D))/(2*a);
-            result.x2 = (-b - sqrt(D))/(2*a);
+            result.code = TWO_ROOTS;
+            double D_sqrt = sqrt(D);
+            result.x1 = (-b + D_sqrt)/(2*a);
+            result.x2 = (-b - D_sqrt)/(2*a);
         }
     }
+    //fix -0 case
+    result.x1 = fixMinusZero(result.x1);
+    result.x2 = fixMinusZero(result.x2);
     return result;
 }
 
