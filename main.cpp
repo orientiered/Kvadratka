bc7f67f4bf0d7a365fcdf873a62fbda17bdf9cd6
orientiered <da.morgachev@mail.ru>
2024-08-21 17:47:05 +0300

    Added some functions, another update of input system

--- main.cpp
+++ main.cpp
@@ -9,8 +9,10 @@
 #include <ctype.h>
 
 const double EPSILON = 1e-10; //constant for comparing floats
+const int EOT = 4;
+const int SUB = 26;
 #define EXIT_BAD_INPUT 1 //exit when input can't be parsed correctly
-
+#define swapDouble(a, b) do{double c = b; b = a; a = c;}while(0)
 
 /// @brief Exit codes used in solution_t struct
 /// @see solution
@@ -22,7 +24,13 @@ enum solutionCode {
     BLANK_ROOT = -1     ///< This code is used when solution_t is initialized
 };
 
-
+/// @brief Error codes which can be used in many functions
+enum error {
+    BAD_EXIT,           ///< Function handled fail inside it correctly
+    GOOD_EXIT,          ///< Function worked correctly and all is as expected
+    STRANGE_EXIT,        ///< Exit in condinitions that can't be reached
+    FAIL                ///< Emergency exit
+};
 
 /*!
     \brief Struct that stores solutions
@@ -42,6 +50,12 @@ typedef struct quadraticEquation {
     solution_t answer;  //< structure with exit code and answers
 } quadraticEquation_t;
 
+
+/// @brief Struct which stores data for unit-testing
+typedef struct unitTest {
+    quadraticEquation_t inputData; //< Struct with test coefficients
+    solution_t expectedData;       //< Struct with expected solutions
+} unitTest_t;
 
 
 /*!
@@ -91,9 +105,11 @@ quadraticEquation_t* solveQuadratic(quadraticEquation_t* equation);
     @param[in] argc Number of cmd arguments
     @param[in] argv Cmd arguments
 
-    @return 0 if problem occured, 1 if input is readed correctly
+    @return Appropriate error code
+
+    @see error
 */
-int scanCoefficients(quadraticEquation_t* equation, int argc, char *argv[]);
+enum error scanCoefficients(quadraticEquation_t* equation, int argc, char *argv[]);
 
 
 /*!
@@ -105,13 +121,57 @@ void printAnswer(const quadraticEquation_t* equation);
 
 
 /*!
+    @brief Runs unit-tests
 
+    @return Number of failed test; 0 if all tests are passed
+*/
+int unitTesting();
+
+
+/*!
+    @brief Runs exactly one test
+
+    @param[in] test Struct with test data and expected data
+
+    @return 1 if test is passed, 0 if failed
+*/
+enum error runTest(unitTest_t test);
+
+
+
+/*!
+    @brief Safe double comparison
+
+    @param[in] a First number
+    @param[in] b Second number
+
+    @return -1 if a < b, 0 if a == b, 1 if a > b
+
+    @see EPSILON
+    Using EPSILON const to compare
+*/
+int cmpDouble(const double a, const double b);
+
+
+/*!
+    @brief Checks double on zero
+
+    @return 1 if a == 0 else 0
+*/
+int isZero(const double a);
+
+
+/*!
+    @brief Clears stdin buffer using getchar() until end of line
 */
 void flushScanfBuffer();
 
 int main(int argc, char *argv[]) {
     printf("# Quadratic equation solver\n# orientiered 2024\n");
 
+    int failedTest = 0;
+    if ((failedTest = unitTesting()) != 0)
+     printf("UNIT TESTING FAILED on test %d\n", failedTest);
 
     quadraticEquation_t equation = {NAN, NAN, NAN, BLANK_SOLUTION};
     if (!scanCoefficients(&equation, argc, argv)) {
@@ -127,8 +187,8 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-
-int scanCoefficients(quadraticEquation_t* equation, int argc, char *argv[]) {
+//Enum error { GOOD_ENDING=1, BAD_ENDING=0, STRANGE_ENDING=};
+enum error scanCoefficients(quadraticEquation_t* equation, int argc, char *argv[]) {
     assert(equation);
     assert(argv);
 
@@ -136,48 +196,57 @@ int scanCoefficients(quadraticEquation_t* equation, int argc, char *argv[]) {
     if (argc == 4) { //getting input from cmd args
         if (sscanf(*++argv, "%lf", &(equation->a)) != 1) {
             printf("Can't read first coefficient\n");
-            return 0;
+            return BAD_EXIT;
         }
         if (sscanf(*++argv, "%lf", &(equation->b)) != 1) {
             printf("Can't read second coefficient\n");
-            return 0;
+            return BAD_EXIT;
         }
         if (sscanf(*++argv, "%lf", &(equation->c)) != 1) {
             printf("Can't read third coefficient\n");
-            return 0;
+            return BAD_EXIT;
         }
-        return 1;
+        return GOOD_EXIT;
     } else {
         printf("Enter coefficients of equation ax^2 + bx + c = 0\n");
         double *coeffsArray[] = {&(equation->a), &(equation->b), &(equation->c)};
         int index = 0;
-        int remainingTries = 3;
 
-        for (; index < 3 && remainingTries > 0; ) {
-            if (scanf("%lf", coeffsArray[index]) != 1) {
-                printf("Wrong input format\n");
-                index = 0; remainingTries--;
-                flushScanfBuffer();
+        const int MAX_BUFFER_LEN = 1000;
+        char strBuffer[MAX_BUFFER_LEN];
+
+        int scanfStatus = 0;
+        for (; index < 3; ) {
+            if ((scanfStatus = scanf("%lf", coeffsArray[index])) == EOF) {
+                break;
+            } else if (scanfStatus != 1) {
+                printf("Wrong input format. \n");
+                scanf("%s", strBuffer);
+                printf("Can't convert \"%s\" to double, skipping\n", strBuffer);
+                //flushScanfBuffer();
             } else {
                 int c = 0;
                 if (!isspace(c = getchar())) {
-                    ungetc((c == EOF) ? 0: c, stdin);
-                    printf("Wrong input format\n");
-                    index = 0;
-                    flushScanfBuffer();
+                    ungetc(c, stdin);
+                    scanf("%s", strBuffer);
+                    printf("Wrong input format. \n");
+                    printf("Can convert to double, but \"%s\" is right after the number.\n", strBuffer);
+                    //flushScanfBuffer();
                 } else index++;
             }
         }
-        if (index == 3) return 1;
-        return 0;
+        if (index == 3) return GOOD_EXIT;
+        else return BAD_EXIT;
     }
-    return 1;
+    return STRANGE_EXIT;
 }
 
 void flushScanfBuffer() {
     int c = 0;
-    while ((c = getchar()) != '\n' && c != '\0' && c != EOF);
-    ungetc( (c == EOF) ? '\0' : c, stdin);
+    while (!isspace(c = getchar()) && c != '\n' && c != '\0' && c != EOF && c != EOT && c != SUB){}
+    //printf("\n%p %d\n", stdin, c);
+    //ungetc( (c == EOF) ? '\0' : c, stdin);
+    //printf("%p %d\n", stdin, c);
 }
 
 void printKvadr(const quadraticEquation_t* equation) {
@@ -189,22 +258,22 @@ void printKvadr(const quadraticEquation_t* equation) {
             b = equation->b,
             c = equation->c;
 
-    if (fabs(a) > EPSILON) { //if not zero
+    if (!isZero(a)) { //if not zero
         if (a < 0) printf("-"); //sign
-        if (fabs(fabs(a)-1) > EPSILON) printf("%g", fabs(a)); //1x^2 is the same as x^2
+        if (!cmpDouble(fabs(a), 1)) printf("%g", fabs(a)); //1x^2 is the same as x^2
         printf("x^2 ");
         printedBefore = 1;
     }
 
-    if (fabs(b) > EPSILON) {
+    if (!isZero(b)) {
         if (printedBefore) printf((b < 0) ? "- " : "+ ");
-        if (fabs(fabs(b)-1) > EPSILON) printf("%g", printedBefore ? fabs(b) : b);
+        if (!cmpDouble(fabs(b), 1)) printf("%g", printedBefore ? fabs(b) : b);
         //if a == 0 => we should print -b, not "- b"
         printf("x ");
         printedBefore = 1;
     }
 
-    if (!(printedBefore && fabs(c) < EPSILON)) { //x + 0 <=> x
+    if (!(printedBefore && isZero(c))) { //x + 0 <=> x
         if (printedBefore) printf((c < 0) ? "- %g " : "+ %g ", fabs(c));
         else printf("%g ", c);
     }
@@ -238,8 +307,98 @@ void printAnswer(const quadraticEquation_t* equation) {
 }
 
 
+int unitTesting() {
+    unitTest_t testData[] = {
+        {
+            {0, 0, 0, BLANK_SOLUTION},
+            {INF_ROOTS, 0, 0}
+        },
+        {
+            {0, 0, 2.43, BLANK_SOLUTION},
+            {ZERO_ROOTS, 0, 0}
+        },
+        {
+            {1, 0, 3, BLANK_SOLUTION},
+            {ZERO_ROOTS, 0, 0}
+        },
+        {
+            {1, 0, -16, BLANK_SOLUTION},
+            {TWO_ROOTS, -4, 4}
+        }
+    };
+    const int testSize = sizeof(testData) / sizeof(unitTest_t);
+
+    for (int testIndex = 0; testIndex < testSize; testIndex++) {
+        if (runTest(testData[testIndex]) != GOOD_EXIT)
+            return testIndex + 1;
+        else {
+            printf("Test #%d passed\n", testIndex+1);
+        }
+    }
+    return 0;
+}
+
+
+
+enum error runTest(unitTest_t test) {
+    solveQuadratic(&test.inputData);
+    solution_t result = test.inputData.answer;
+
+    if (result.code != test.expectedData.code) { //checking exit code first
+        printKvadr(&test.inputData); //print equation
+        printf("Exit code doesn't match: expected %d, got %d\n",
+                test.expectedData.code, result.code);
+        return BAD_EXIT;
+
+    } else {
+
+        switch (result.code) {
+            case INF_ROOTS:
+            case BLANK_ROOT:
+            case ZERO_ROOTS:
+                return GOOD_EXIT;
+            case ONE_ROOT:
+                if(cmpDouble(result.x1, test.expectedData.x1) != 0) {
+                    printKvadr(&test.inputData); //print equation
+                    printf( "Answers doesn't match: expected x = %lf, got x = %lf\n",
+                    test.expectedData.x1, result.x1);
+                    return BAD_EXIT;
+                } else
+                    return GOOD_EXIT;
+            case TWO_ROOTS:
+                if (result.x1 > result.x2)
+                    swapDouble(result.x1, result.x2);
+                if (cmpDouble(result.x1, test.expectedData.x1) != 0 || cmpDouble(result.x2, test.expectedData.x2) != 0) {
+                    printKvadr(&test.inputData); //print equation
+                    printf( "Answers doesn't match: expected x1 = %lf, x2 = %lf\n"
+                            "Got x1 = %lf, x2 = %lf\n",
+                            test.expectedData.x1, test.expectedData.x2, result.x1, result.x2);
+                    return BAD_EXIT;
+                } else
+                    return GOOD_EXIT;
+            default:
+                return BAD_EXIT;
+        }
+    }
+    return STRANGE_EXIT;
+}
+
+
+int cmpDouble(const double a, const double b) {
+    assert(!isinf(a));
+    assert(!isinf(b));
+    if (isnan(a) && isnan(b)) return 0;
+    if (fabs(a - b) < EPSILON) return 0;
+    else return (a < b) ? -1 : 1;
+}
+
+
+int isZero(const double a) {
+    return fabs(a) < EPSILON;
+}
+
 quadraticEquation_t* solveQuadratic(quadraticEquation_t* equation) {
-    assert(equation != NULL);
+    assert(equation);
 
     double  a = equation->a,
             b = equation->b,
@@ -249,14 +408,14 @@ quadraticEquation_t* solveQuadratic(quadraticEquation_t* equation) {
     assert(isfinite(b));
     assert(isfinite(c));
 
-    if (fabs(a) < EPSILON) { //checking for zeros in coefficients; we divide only by a, so this check is essential
+    if (isZero(a)) { //checking for zeros in coefficients; we divide only by a, so this check is essential
         //a = 0
-        if (fabs(b) < EPSILON) {
+        if (isZero(b)) {
             //b = 0
-            if (fabs(c) < EPSILON) { // 0 = 0
+            if (isZero(c)) { // 0 = 0
                 equation->answer.code = INF_ROOTS;
             } else {
-                printf("Equation can't be solved\n");
+                //printf("Equation can't be solved\n");
                 equation->answer.code = ZERO_ROOTS;
             }
         } else {
@@ -265,17 +424,17 @@ quadraticEquation_t* solveQuadratic(quadraticEquation_t* equation) {
         }
     } else {
         const double D = b*b - 4*a*c; //calculating discriminant
-        if (fabs(D) < EPSILON) {            //D = 0
+        if (isZero(D)) {            //D = 0
             equation->answer.code = ONE_ROOT;
             equation->answer.x1 = -b / (2*a);
         } else if (D < 0) {
             equation->answer.code = ZERO_ROOTS;              //D < 0
-            printf("Equation can't be solved in R: D = %g < 0\n", D);
+            //printf("Equation can't be solved in R: D = %g < 0\n", D);
         } else {                            //D > 0
             equation->answer.code = TWO_ROOTS;
             const double D_sqrt = sqrt(D);
-            equation->answer.x1 = (-b + D_sqrt)/(2*a);
-            equation->answer.x2 = (-b - D_sqrt)/(2*a);
+            equation->answer.x1 = (-b - D_sqrt)/(2*a);
+            equation->answer.x2 = (-b + D_sqrt)/(2*a);
         }
     }
     //fix -0 case
@@ -289,5 +448,5 @@ quadraticEquation_t* solveQuadratic(quadraticEquation_t* equation) {
 }
 
 double fixMinusZero(const double num) {
-    return (fabs(num) < EPSILON) ? fabs(num) : num;
+    return (isZero(num)) ? fabs(num) : num;
 }
